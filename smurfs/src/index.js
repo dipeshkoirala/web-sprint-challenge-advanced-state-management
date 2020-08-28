2ddfbecf4411ec32a66fe4914be796da6d38e653
dipeshkoirala <dipeshkoirala@gmail.com>
2020-08-28 19:26:53 -0400

    Beginning work env setup

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
+/* - [*] **Open** `src/index.js` to make sure that your app is ready to roll with the proper middleware. */
+import thunk from 'redux-thunk'
+import {  applyMiddleware } from "redux";
+
+
 ReactDOM.render(<App />, document.getElementById("root"));
